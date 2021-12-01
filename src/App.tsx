a8c37d65798b244c63335e0531cc9728d9324272
pavel-s <sergeevps@gmail.com>
2021-12-01 14:56:58 +0500

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
@@ -8,31 +7,31 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign='center' fontSize='xl'>
+        <Grid minH='100vh' p={3}>
+          <ColorModeSwitcher justifySelf='flex-end' />
+          <VStack spacing={8}>
+            <Text>
+              Edit <Code fontSize='xl'>src/App.tsx</Code> and save to reload.
+            </Text>
+            <Link
+              color='teal.500'
+              href='https://chakra-ui.com'
+              fontSize='3xl'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};
